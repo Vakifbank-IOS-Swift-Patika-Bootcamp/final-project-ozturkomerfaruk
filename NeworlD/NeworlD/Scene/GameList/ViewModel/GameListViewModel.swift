f7ea5cafcc8dd5f8a92f8f2503690dc1eadac2ce
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-12-08 15:19:22 +0300

    Game List Service is done & I started to design to GameListView

--- NeworlD/NeworlD/Scene/GameList/ViewModel/GameListViewModel.swift
+++ NeworlD/NeworlD/Scene/GameList/ViewModel/GameListViewModel.swift
@@ -7,6 +7,44 @@
 
 import Foundation
 
-class GameListViewModel {
+protocol GameListViewModelProtocol {
+    var delegate: GameListViewModelDelegate? { get set }
+    
+    func fetchGameList()
+    func getGameListCount() -> Int
+    func getGame(at index: Int) -> GameModel?
+}
+
+protocol GameListViewModelDelegate: AnyObject {
+    func gamesLoaded()
+    func gamesFailed(error: Error)
+    
+    // func preFetch()
+    // func postFetch()
+}
+
+class GameListViewModel: GameListViewModelProtocol {
+    weak var delegate: GameListViewModelDelegate?
+    private var games: [GameModel]?
+    
+    func fetchGameList() {
+        Client.getGameList(page: 1, pageSize: 5) { [weak self] games, error in
+            guard let self = self else { return }
+            if let error = error {
+                self.delegate?.gamesFailed(error: error)
+            }
+            self.games = games
+            self.delegate?.gamesLoaded()
+        }
+    }
+    
+    func getGameListCount() -> Int {
+        return games?.count ?? 0
+    }
+    
+    func getGame(at index: Int) -> GameModel? {
+        return games?[index]
+    }
+    
     
 }
